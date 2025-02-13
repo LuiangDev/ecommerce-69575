21888434e3e97a8c1c98a319fdc7b302dbea1799
Luis Angel <angel.quispe.navarro@outlook.com>
2025-02-13 15:40:38 -0500

    Actualizacion de Entrega 01

---
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+import { TiShoppingCart } from "react-icons/ti";
+export const CartWidget = () => {
+  return (
+    <div>
+      <TiShoppingCart />
+      <h3>2</h3>
+    </div>
+  );
+};
---
+++ src/components/common/productCard/ProductCard.jsx
@@ -0,0 +1,13 @@
+import "./productCard.css";
+export const ProductCard = ({ title, price }) => {
+
+  return (
+    <div className="container-card">
+      <h3> {title} </h3>
+      <h3> {price} </h3>
+    </div>
+  );
+};
+
+
+
